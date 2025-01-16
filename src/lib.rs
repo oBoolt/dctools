e809b293e5d6fcf9a4fd20e9d7d35f2794a849f2
oBoolt <bernardo.patto.pinheiro@gmail.com>
2025-01-16 14:09:01 -0300

    refactor(config): add a module to config related functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,3 @@
-use anyhow::Context;
-use serde::Deserialize;
-use std::{
-    fs,
-    io::{self, Read},
-    path,
-};
+pub mod config;
 
-#[derive(Deserialize, Debug)]
-pub struct Config {
-    pub token: String,
-}
 
-impl Config {
-    pub fn load_config<P: AsRef<path::Path>>(path: P) -> anyhow::Result<Self> {
-        let file = fs::File::open(path).context("Failed to open config file")?;
-        let mut reader = io::BufReader::new(file);
-        let mut content = String::new();
-        reader
-            .read_to_string(&mut content)
-            .context("Failed to read config file content")?;
-
-        let config: Self = toml::from_str(&content)
-            .context("Failed to deserialize config file content to config struct")?;
-
-        Ok(Self {
-            token: config.token,
-        })
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use discord_spammer::config::Config;
 use std::process;
 
 fn main() {
-    let config = match discord_spammer::Config::load_config("./config.toml") {
+    let config = match Config::load_config("./config.toml") {
         Ok(x) => x,
         Err(e) => {
             println!("[\x1b[31mError\x1b[0m] {}", e);
             process::exit(1);
         }
     };
+    dbg!(config);
 }
