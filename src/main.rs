88d6b803bc4a9c9f3eb2209b61ec655789c1b22c
oBoolt <bernardo.patto.pinheiro@gmail.com>
2025-01-16 14:03:31 -0300

    feat(config): able to parse a config file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let config = match discord_spammer::Config::load_config("./config.toml") {
+        Ok(x) => x,
+        Err(e) => {
+            println!("[\x1b[31mError\x1b[0m] {}", e);
+            process::exit(1);
+        }
+    };
 }
