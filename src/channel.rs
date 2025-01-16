dfe45567f79eedfc354146b3a29fe1a1ac28a3af
oBoolt <bernardo.patto.pinheiro@gmail.com>
2025-01-16 15:44:58 -0300

    feat(channel): create struct for the message payload

---
+++ src/channel.rs
@@ -0,0 +1,44 @@
+use anyhow::Context;
+use rand::Rng;
+use serde::Serialize;
+
+use crate::cli::Args;
+
+#[derive(Serialize, Debug)]
+pub struct MessagePayload {
+    pub content: String,
+    pub flags: u8,
+    pub mobile_network_type: String,
+    pub nonce: String,
+    pub tts: bool,
+}
+
+impl MessagePayload {
+    pub fn new(args: Args) -> anyhow::Result<Self> {
+        let nonce = Self::get_nonce().context("Failed to generate nonce")?;
+
+        Ok(Self {
+            content: args.content,
+            flags: 0,
+            mobile_network_type: String::from("unknown"),
+            nonce,
+            tts: false,
+        })
+    }
+
+    fn get_nonce() -> anyhow::Result<String> {
+        let mut result = String::new();
+        let mut rng = rand::thread_rng();
+        let numbers = "1234567890";
+
+        for _ in 0..19 {
+            let char = numbers
+                .chars()
+                .nth(rng.gen_range(0..numbers.len()))
+                .context("Failed to create random number")?;
+            result.push(char);
+        }
+
+        Ok(result)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
+pub mod channel;
 pub mod cli;
 pub mod config;
